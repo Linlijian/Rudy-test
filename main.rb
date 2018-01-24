a189d94d0be740948ea86a902236dcdd824a34f4
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-01-24 22:13:17 +0700

    Test Case  5 String [pass 4]

--- main.rb
+++ main.rb
@@ -2,7 +2,28 @@
 require_relative 'THTokenizer'
 
 s="จึงเรียนมาเพื่อทราบ"
+s1="ไม่รู้"
+s2="หรอยจัง"
+s3="พอตัดคำยาวมักหลอกตา"
+s4="ไม่มีคำว่าผิดสำหรับคนพึ่งเริ่ม"
+
 test = THCuster.new(s)
+puts test.THTokenizer
+puts test.getTHTokenizer
+
+test = THCuster.new(s1)
+puts test.THTokenizer
+puts test.getTHTokenizer
+
+test = THCuster.new(s2)
+puts test.THTokenizer
+puts test.getTHTokenizer
+
+test = THCuster.new(s3)
+puts test.THTokenizer
+puts test.getTHTokenizer
+
+test = THCuster.new(s4)
 puts test.THTokenizer
 puts test.getTHTokenizer
 
